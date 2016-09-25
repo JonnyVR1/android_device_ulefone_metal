aee7901df737e39b541350e1a4c5b9bc8b4532ad
Lucky76 <dpluca.76@gmail.com>
2016-09-25 11:45:35 +0200

    Initial commit
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := metal,METAL,ulefone,Ulefone,wtk6753_65u_m0
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+#Use dlmalloc instead of jemalloc for mallocs
+MALLOC_IMPL := dlmalloc
