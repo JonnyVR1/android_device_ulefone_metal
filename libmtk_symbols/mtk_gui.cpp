392e2475c5319123d869c7d67d14ba1000c228be
Jonathon Fitch <lightman1@virginmedia.com>
2017-02-21 10:01:26 +0000

    Reorganise libmtk_symbols
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

--- libmtk_symbols/mtk_gui.cpp
+++ libmtk_symbols/mtk_gui.cpp
@@ -1,9 +1,5 @@
 #include <stdint.h>
 
 extern "C" {
-    void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
-
-    void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
-        _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
-    }
+    void _ZN7android13GraphicBufferC1Ejjij(uint32_t inWidth, uint32_t inHeight, int32_t inFormat, uint32_t inUsage);
 }
